19b726fba27a08d1e09920fca9940d35879409cd
Hai-dozo <risaishide@gmail.com>
2020-01-22 09:03:59 +0900

    -

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 // import App from './App';
-import Body from './Body';
+import Qa from './Qa';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Body />, document.getElementById('body'));
+ReactDOM.render(<Qa />, document.getElementById('qa'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
