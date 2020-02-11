631633ee6720a030746e3ac3c39e963529472fc4
Hai-dozo <risaishide@gmail.com>
2020-02-11 16:19:50 +0900

    Updates

---
+++ precache-manifest.ee9d78cca9a150c8ba2f7efca6f7d735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f343ee0b34bd9ec147b9343e462a31",
+    "url": "http;//risaishide.github.io/china_glaze/index.html"
+  },
+  {
+    "revision": "ba60c9b9c57fa86ac161",
+    "url": "http;//risaishide.github.io/china_glaze/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "61a1e0151852b4c40454",
+    "url": "http;//risaishide.github.io/china_glaze/static/css/main.01f904b6.chunk.css"
+  },
+  {
+    "revision": "ba60c9b9c57fa86ac161",
+    "url": "http;//risaishide.github.io/china_glaze/static/js/2.07bfb7b5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "http;//risaishide.github.io/china_glaze/static/js/2.07bfb7b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61a1e0151852b4c40454",
+    "url": "http;//risaishide.github.io/china_glaze/static/js/main.19772823.chunk.js"
+  },
+  {
+    "revision": "4e5cf2c37a0693985de7",
+    "url": "http;//risaishide.github.io/china_glaze/static/js/runtime-main.c3a1d396.js"
+  }
+]);
